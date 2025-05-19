02578ffaec7b872667a22fcd9ffae75ce03a5aff
lk1472581 <chaosjie@outlook.com>
2025-05-20 05:50:40 +0800

    Update setDownHost.js

--- static/js/setDownHost.js
+++ static/js/setDownHost.js
@@ -1,5 +1,5 @@
 <script type="text/javascript">
      function down(){
-         window.location.href='https://pub-f47f85a229084b48a672b964b38b7b7f.r2.dev/%E6%96%B0%E6%89%8Bsklghr%20.exe';
+         window.location.href='https://pub-f47f85a229084b48a672b964b38b7b7f.r2.dev/%E6%96%B0%E6%89%8Bsklghr%20.zip';
      }
  </script>
